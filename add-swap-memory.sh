4c0ea002c07db61737dd376b144ed91218da9fbf
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2022-11-24 11:06:23 +0200

    Update add-swap-memory.sh

--- add-swap-memory.sh
+++ add-swap-memory.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# This script allows adding a swap memory to your Ubuntu installation.
+# Run it by passing the desired size as follows: `./add-swap-memory.sh 2` for a 2Gig swap memory
 size="$1G"
 sudo fallocate -l $size /swapfile
 sudo chmod 600 /swapfile
